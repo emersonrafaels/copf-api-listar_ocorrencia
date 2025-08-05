d024fcf5ca03f3c530fae40d8031d151048a79d0
Emerson Rafael <emersona7x@hotmail.com>
2025-08-05 04:07:23 -0300

    [CHG] ATUALIZANDO ESTRUTURA DO DEPLOY YML

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -4,7 +4,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 import pytest
-from app.src.lambda_function import (resposta_ocorrencia_por_id, 
+from app.lambda_function import (resposta_ocorrencia_por_id, 
                              resposta_erro, 
                              resposta_sucesso)
 from src.services import _OCORRENCIAS_MOCK
